29677d0cf9a105542329db7cc1d646b87f3908e4
xAkii <58516278+xAkii@users.noreply.github.com>
2020-03-12 21:01:39 +0100

    Add files via upload

---
+++ pingpong/pong/Ball.cs
@@ -0,0 +1,78 @@
+﻿using System;
+using System.Windows.Forms;
+using System.Drawing;
+
+namespace Pong {
+    public class Ball {
+        PongForm form;
+        private PictureBox ballPictureBox, ballModel;
+        Random rand = new Random();
+        Player leftsidePlayer, rightsidePlayer;
+        int xSpeed, ySpeed;
+
+        public Ball(PongForm form, PictureBox aBallModel, Player leftsidePlayer, Player rightsidePlayer) {
+            this.form = form;
+            ballModel = aBallModel;
+            this.ballPictureBox = new PictureBox();
+            this.ballPictureBox.Size = aBallModel.Size;
+            this.ballPictureBox.Image = aBallModel.Image;
+            form.Controls.Add(ballPictureBox);
+            this.leftsidePlayer = leftsidePlayer;
+            this.rightsidePlayer = rightsidePlayer;
+            ResetBall();
+        }
+
+        internal bool ProcessMove() {
+            DoMove();
+
+            var bottom = PongWorldInfo.bottomOfWorld - ballPictureBox.Height;
+            if(ballPictureBox.Location.Y >= bottom || ballPictureBox.Location.Y <= PongWorldInfo.topOfWorld) {
+                ySpeed *= -1;
+            }
+
+            if(ballPictureBox.Location.X <= PongWorldInfo.leftOfWorld) {
+                Score(leftsidePlayer);
+                return true;
+            } else if(ballPictureBox.Location.X >= PongWorldInfo.rightOfWorld - ballPictureBox.Width) {
+                Score(rightsidePlayer);
+                return true;
+            }
+
+            if(leftsidePlayer.paddle.Bounds.IntersectsWith(ballPictureBox.Bounds)
+                || rightsidePlayer.paddle.Bounds.IntersectsWith(ballPictureBox.Bounds)) {
+                xSpeed *= -1;
+
+                form.ballList.Add(new Ball(form, ballModel, leftsidePlayer, rightsidePlayer));
+
+                while(leftsidePlayer.paddle.Bounds.IntersectsWith(ballPictureBox.Bounds)
+                    || rightsidePlayer.paddle.Bounds.IntersectsWith(ballPictureBox.Bounds)) {
+                    DoMove();
+                }
+            }
+
+            return false;
+        }
+
+        private int DoMove() {
+            var bottom = PongWorldInfo.bottomOfWorld - ballPictureBox.Height;
+            ballPictureBox.Location = new Point(ballPictureBox.Location.X + xSpeed,
+                Math.Max(PongWorldInfo.topOfWorld, Math.Min(bottom, ballPictureBox.Location.Y + ySpeed))
+                );
+            return bottom;
+        }
+
+        private void Score(Player winningPlayer) {
+            winningPlayer.score++;
+            form.Controls.Remove(ballPictureBox);
+        }
+
+        private void ResetBall() {
+            ballPictureBox.Location = new Point((PongWorldInfo.leftOfWorld + PongWorldInfo.rightOfWorld) / 2,
+                            (PongWorldInfo.topOfWorld + PongWorldInfo.bottomOfWorld) / 2);
+            do {
+                xSpeed = rand.Next(-3, 3);
+                ySpeed = rand.Next(-3, 3);
+            } while(Math.Abs(xSpeed) + Math.Abs(ySpeed) <= 3 || Math.Abs(xSpeed) <= 1);
+        }
+    }
+}
---
+++ pingpong/pong/Player.cs
@@ -0,0 +1,81 @@
+﻿using System;
+using System.Windows.Forms;
+using System.Drawing;
+
+namespace Pong {
+    public class Player {
+        const int movementSpeed = 3;
+
+        public bool isUpPressed, isDownPressed;
+
+        public PictureBox paddle;
+        Label scoreLabel;
+        bool? wasGoingUpLastTick;
+        int numberOfTicksGoingInTheSameDirection;
+
+        int _score;
+        public int score {
+            get {
+                return _score;
+            }
+            set {
+                _score = value;
+                scoreLabel.Text = score.ToString();
+            }
+        }
+
+        public Player(PictureBox aPaddle1, Label scoreLabel) {
+            this.paddle = aPaddle1;
+            this.scoreLabel = scoreLabel;
+        }
+
+        internal void ProcessMove() {
+            bool? goingUp = null;
+
+            if(isUpPressed) {
+                goingUp = true;
+            }
+
+            if(isDownPressed) {
+                if(goingUp.HasValue) {
+                    goingUp = null;
+                } else {
+                    goingUp = false;
+                }
+            }
+
+            if(wasGoingUpLastTick.HasValue) {
+                if(!goingUp.HasValue) {
+                    wasGoingUpLastTick = null;
+                    numberOfTicksGoingInTheSameDirection = 0;
+                } else if(wasGoingUpLastTick.Value == goingUp.Value) {
+                    numberOfTicksGoingInTheSameDirection++;
+                } else {
+                    wasGoingUpLastTick = goingUp;
+                    numberOfTicksGoingInTheSameDirection = 1;
+                }
+            } else if(goingUp.HasValue) {
+                wasGoingUpLastTick = goingUp;
+                numberOfTicksGoingInTheSameDirection = 1;
+            }
+
+            DoMove(goingUp);
+        }
+
+        private void DoMove(bool? goingUp) {
+            if(goingUp.HasValue) {
+                var speed = (int)Math.Round(movementSpeed * ((float)numberOfTicksGoingInTheSameDirection / 10));
+                if(goingUp.Value) {
+                    speed *= -1;
+                }
+
+                paddle.Location = new Point(paddle.Location.X,
+                    Math.Max(PongWorldInfo.topOfWorld,
+                        Math.Min(PongWorldInfo.bottomOfWorld - paddle.Height
+                        , paddle.Location.Y + speed)
+                        )
+                    );
+            }
+        }
+    }
+}
